f2887296a0fcf1248dbfb9e37d960c049c9ccba2
Nguyễn Hoàng Thiên <149929660+Thienx2004@users.noreply.github.com>
2025-10-02 21:57:14 +0700

    fix getHomepage

--- src/main/java/com/example/onlyfanshop_be/service/IProductService.java
+++ src/main/java/com/example/onlyfanshop_be/service/IProductService.java
@@ -2,6 +2,7 @@ package com.example.onlyfanshop_be.service;
 
 import com.example.onlyfanshop_be.dto.response.ApiResponse;
 import com.example.onlyfanshop_be.dto.ProductDetailDTO;
+import com.example.onlyfanshop_be.dto.response.HomepageResponse;
 
 public interface IProductService {
     public ApiResponse<HomepageResponse> getHomepage(String keyword, Integer categoryId, Integer brandId, int page, int size, String sortBy, String order);
