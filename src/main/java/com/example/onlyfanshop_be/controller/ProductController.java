0ee8aabe45c330bd758b61217309a143aca8e0ec
NTT-DevFPT <thanhtai10903@gmail.com>
2025-10-03 09:43:56 +0700

    Product Details

--- src/main/java/com/example/onlyfanshop_be/controller/ProductController.java
+++ src/main/java/com/example/onlyfanshop_be/controller/ProductController.java
@@ -1,17 +1,12 @@
 package com.example.onlyfanshop_be.controller;
 
 import com.example.onlyfanshop_be.dto.response.ApiResponse;
-import com.example.onlyfanshop_be.service.IProductService;
 import com.example.onlyfanshop_be.dto.ProductDetailDTO;
+import com.example.onlyfanshop_be.service.IProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/product")
@@ -34,9 +29,9 @@ public class ProductController {
                 .body(response);
     }
 
-    @GetMapping("/{id}")
-    public ResponseEntity<ApiResponse<ProductDetailDTO>> getProductDetail(@PathVariable("id") Integer id) {
-        ApiResponse<ProductDetailDTO> response = iProductService.getProductDetail(id);
+    @GetMapping("/detail/{productId}")
+    public ResponseEntity<ApiResponse<ProductDetailDTO>> getProductDetail(@PathVariable Integer productId) {
+        ApiResponse<ProductDetailDTO> response = iProductService.getProductDetail(productId);
         return ResponseEntity
                 .status(HttpStatus.OK)
                 .body(response);
